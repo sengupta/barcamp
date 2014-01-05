d42123064abdff30fcf74da60d75a260799ff57d
Aditya Sengupta <aditya@sengupta.me>
2014-01-05 22:47:58 +0530

    Implemented basic registration view

--- core/views.py
+++ core/views.py
@@ -1,6 +1,13 @@
 from django.views.generic import TemplateView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
+from .forms import RegisterForm
+from .models import UserProfile
 
 class HomeView(TemplateView):
     def get(self, request):
@@ -10,9 +17,33 @@ class HomeView(TemplateView):
     def post(self, request):
         pass
 
-class LoginView(TemplateView):
+class RegisterView(TemplateView):
+    template_name = "auth/register.html"
     def get(self, request):
-        pass
+        register_form = RegisterForm()
+        return self.render_to_response({
+                    'register_form': register_form,
+                    })
+
+    def post(self, request):
+        register_form = RegisterForm(data=request.POST)
+        if not register_form.is_valid():
+            return self.render_to_response({
+                'register_form': register_form
+                })
+        data = register_form.cleaned_data
+        name = data['name']
+        email = data['email']
+        password = data['password']
+
+        profile=UserProfile.profiles.create_profile(
+                name=name,
+                email=email,
+                password=password
+                )
+        user = authenticate(username=profile.user.username, password=password)
+        login(request, user)
+        return HttpResponseRedirect(reverse('dashboard'))
 
     def post(self, request):
         pass
