9a17fdc56a9495a372f6eb5d8319edb7c914a3f5
Aditya Sengupta <aditya@sengupta.me>
2014-01-05 19:17:11 +0530

    Base outline for core app

---
+++ core/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class BaseModel(models.Model):
+    class Meta:
+        abstract=True
+    created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+class Camp(BaseModel):
+    name = models.CharField(max_length=255)
+    start = models.DateTimeField(null=True, blank=True)
+    end = models.DateTimeField(null=True, blank=True)
+    venue_address = models.TextField(null=True, blank=True)
+
+    def __unicode__(self):
+        return self.name
+
+class UserProfile(BaseModel):
+    user = models.OneToOneField(User)
+
+class Session(BaseModel):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    speaker = models.ManyToManyField(UserProfile, related_name="speakers")
+    camp = models.ForeignKey(Camp)
+    enabled = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return self.title
---
+++ core/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django
+
+class CampView(TemplateView):
+    pass
+
+class SessionView(TemplateView):
+    pass
+
+class SessionCreateView(TemplateView):
+    pass
+
+class SessionEditView(TemplateView):
+    pass
+
+class ProfileView(TemplateView):
+    pass
+
+class ProfileCreateView(TemplateView):
+    pass
+
+class ProfileEditView(TemplateView):
+    pass
+
