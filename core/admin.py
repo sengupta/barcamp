059004a3ceddda7ad3fcf57ba5cb916acf61e4b9
Aditya Sengupta <aditya@sengupta.me>
2014-01-05 19:26:33 +0530

    Admin site

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Camp, UserProfile, Session
 
-# Register your models here.
+class CampAdmin(admin.ModelAdmin):
+    pass
+
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
+
+class SessionAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Camp, CampAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(Session, SessionAdmin)
