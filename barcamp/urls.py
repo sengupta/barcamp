b6e87dce1342f36a6fe3a5b40b3b4df10dd12429
Aditya Sengupta <aditya@sengupta.me>
2014-01-07 22:54:27 +0530

    Updated URLs

--- barcamp/urls.py
+++ barcamp/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, include, url
 
 from core.views import HomeView, RegisterView, LoginView, DashboardView, \
-                       CampView, CampListView
+                       CampView, CampListView, \
+                       SessionCreateView, SessionView \
 
 from django.contrib import admin
 admin.autodiscover()
@@ -16,6 +17,8 @@ urlpatterns = patterns('',
     url(r'^register/$', RegisterView.as_view(), name='register'),
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^camps/$', CampListView.as_view(), name='camp_list'),
-    url(r'^(?P<camp_slug>[_a-zA-Z0-9]+)/$', CampView.as_view(), name='camp'),
+    url(r'^(?P<camp>[-_a-zA-Z0-9]+)/(?P<session>[-_a-zA-Z0-9]+)/$', SessionView.as_view(), name='session_view'),
+    url(r'^(?P<camp>[-_a-zA-Z0-9]+)/session/create/$', SessionCreateView.as_view(), name='session_create'),
+    url(r'^(?P<camp>[-_a-zA-Z0-9]+)/$', CampView.as_view(), name='camp'),
     url(r'^$', HomeView.as_view(), name='home_view'),
 )
