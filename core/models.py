7e028c6309609c931e531bc5c882f2427c19c5c5
Aditya Sengupta <aditya@sengupta.me>
2014-02-06 01:36:37 +0530

    Offloaded creation of slug to mixin
    This mixin is used by both, the Session and Camp classes, and can be
    used by any other classes that require slugs

--- core/models.py
+++ core/models.py
@@ -11,23 +11,36 @@ class BaseModel(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
 
-class Camp(BaseModel):
+class SlugMixin(models.Model):
+    class Meta:
+        abstract=True
+
+    slug = models.SlugField(max_length=255, unique=True)
+
+    def create_slug(self, title):
+        slug = slugify(title)[:100]
+        if len(slug.strip()) == 0:
+            slug = md5(str(datetime.datetime.now())).hexdigest()
+        while self.__class__.objects.filter(slug=slug).exists():
+            slug = "{random}-{slug}".format(
+                    random=md5(str(datetime.datetime.now())).hexdigest()[:5],
+                    slug=slug
+                    )
+        self.slug = slug
+
+class Camp(BaseModel, SlugMixin):
     name = models.CharField(max_length=255)
     start = models.DateTimeField(null=True, blank=True)
     end = models.DateTimeField(null=True, blank=True)
     # TODO: Enable timezone support
     venue_address = models.TextField(null=True, blank=True)
-    slug = models.SlugField(max_length=255, unique=True)
 
     def __unicode__(self):
         return self.name
 
-    def create_slug(self):
-        # TODO
-        pass
-
     def save(self, *args, **kwargs):
-        # TODO: generate slug
+        if not self.id: # Created:
+            self.create_slug(self.name)
         super(Camp, self).save(*args, **kwargs)
 
     def clean(self):
@@ -52,29 +65,17 @@ class UserProfile(BaseModel):
     def __unicode__(self):
         return self.name
 
-class Session(BaseModel):
+class Session(BaseModel, SlugMixin):
     title = models.CharField(max_length=100)
     description = models.TextField()
     speaker = models.ForeignKey(UserProfile, related_name="sessions")
     camp = models.ForeignKey(Camp, related_name="sessions")
     enabled = models.BooleanField(default=False)
-    slug = models.SlugField(max_length=255)
 
     def __unicode__(self):
         return self.title
 
-    def create_slug(self):
-        slug = slugify(self.title)[:100]
-        if len(slug.strip()) == 0:
-            slug = md5(str(datetime.datetime.now())).hexdigest()
-        while Session.objects.filter(slug=slug).exists():
-            slug = "{random}-{slug}".format(
-                    random=md5(str(datetime.datetime.now())).hexdigest()[:5],
-                    slug=slug
-                    )
-        self.slug = slug
-
     def save(self, *args, **kwargs):
         if not self.id:
-            self.create_slug()
+            self.create_slug(self.title)
         super(Session, self).save(*args, **kwargs)
