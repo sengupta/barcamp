a07e07cc3dd1eb603580fdeaa33d7d46d007c131
Aditya Sengupta <aditya@sengupta.me>
2014-02-06 02:32:40 +0530

    More info in admin

--- core/admin.py
+++ core/admin.py
@@ -2,13 +2,27 @@ from django.contrib import admin
 from .models import Camp, UserProfile, Session
 
 class CampAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+            'name',
+            'start',
+            'end',
+            'slug',
+            )
 
 class UserProfileAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+            'user',
+            'name',
+            )
 
 class SessionAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+            'title',
+            'speaker',
+            'camp',
+            'enabled',
+            'slug',
+            )
 
 admin.site.register(Camp, CampAdmin)
 admin.site.register(UserProfile, UserProfileAdmin)
