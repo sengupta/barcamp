d70bcde8ecdf29a5417ea3c30595ce14c685fe10
Aditya Sengupta <aditya@sengupta.me>
2014-02-17 10:08:15 +0530

    Access request object in templates

--- barcamp/settings.py
+++ barcamp/settings.py
@@ -48,6 +48,11 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+        'django.contrib.auth.context_processors.auth',
+        'django.core.context_processors.request',
+        )
+
 ROOT_URLCONF = 'barcamp.urls'
 
 WSGI_APPLICATION = 'barcamp.wsgi.application'
